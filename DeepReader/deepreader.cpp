cc9a1e7b449e7ed4d42edf3c26793a24a04d5c24
BirchellE <birchell.eversole@gmail.com>
2017-04-20 09:37:51 -0400

    Adding 100MostCommonWords list
    Added a list of the 100 most common words for use in the RelevantNotes
    function

--- DeepReader/deepreader.cpp
+++ DeepReader/deepreader.cpp
@@ -97,7 +97,8 @@ bool DeepReader::goodNotes() {
 /*
 bool relevantNotes(QStringList words, QStringList notes){
     //We could also use a map structure here to increase efficiency
-    QStringList 100MostCommonWords; //https://en.wikipedia.org/wiki/Most_common_words_in_English
+    QStringList 100MostCommonWords = ({'the','be','to','of','and','a','in','that','have','I','it','for','not','on','with','he','as','you','do','at','this','but','his','by','from','they','we','say','her','she','or','an','will','my','one','all','would','there','their','what','so','up','out','if','about','who','get','which','go','me','when','make','can','like','time','no','just','him','know','take','people','into','year','your','good','some','could','them','see','other','than','then','now','look','only','come','its','over','think','also','back','after','use','two','how','our','work','first','well','way','even','new','want','because','any','these','give','day','most','us'});
+    //https://en.wikipedia.org/wiki/Most_common_words_in_English
     QStringList Qwords = QStringList(words); //new QStringList with elements the same as words
     Qwords.removeDuplicates();
     for (int i = 0; i < words.size(); i++){
